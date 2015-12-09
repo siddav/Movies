e3a851228ad989f5abf3379dcdbccd1b83fc67c6
sidda <vsidda@corp.untd.com>
2015-12-09 17:05:50 +0530

    hiding moviesdb api key

--- app/src/main/java/com/example/sidda/movies/constants/MovieConstants.java
+++ app/src/main/java/com/example/sidda/movies/constants/MovieConstants.java
@@ -1,12 +1,14 @@
 package com.example.sidda.movies.constants;
 
+import com.example.sidda.movies.BuildConfig;
+
 /**
  * Created by sidda on 25/11/15.
  */
 public class MovieConstants {
     public static final String BASE_IMAGE_URL = "http://image.tmdb.org/t/p/w185";
     // this has to be populated with movie db org API KEY
-    public static final String MOVIE_DB_API_KEY = "a900a5353d94ae90dff0ac788f32b649";
+    public static final String MOVIE_DB_API_KEY = BuildConfig.MOVIES_DB_API_KEY;
     public static final String MOVIE_BASE_URL = "https://api.themoviedb.org/3/";
     public static final String MOVIE_POPULAR = "popularity.desc";
     public static final String MOVIE_TOP_RATED = "vote_average.desc";
